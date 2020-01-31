dbc00c5bba546a17f025f95bc9932c5fdda5666c
Asif Shaikh <asif.shaikh@go-jek.com>
2020-01-31 11:53:03 +0530

    adds the rule for making live cell dead, when the number of number of live neighbors is less than 2

--- src/test/kotlin/gameOfLife/rules/GameOfLifeRulesTest.kt
+++ src/test/kotlin/gameOfLife/rules/GameOfLifeRulesTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 
 class GameOfLifeRulesTest {
     @Test
-    fun `given a cell with two live neighbors, should return as live for next iteration`() {
+    fun `given a live cell with two live neighbors, should return as live for next iteration`() {
         val currentCellStatus = LIVE
         val expectedStatus = LIVE
         val neighbors = arrayOf(0,1,1,0,0,0,0,0)
@@ -18,10 +18,34 @@ class GameOfLifeRulesTest {
     }
 
     @Test
-    fun `given a cell with three live neighbors, should return as live for next iteration`() {
+    fun `given a live cell with three live neighbors, should return as live for next iteration`() {
         val currentCellStatus = LIVE
         val expectedStatus = LIVE
         val neighbors = arrayOf(0,1,1,0,0,0,1,0)
+        val rules = GameOfLifeRule()
+
+        val actualStatus = rules.checkIsAlive(currentCellStatus,neighbors)
+
+        assertThat(actualStatus).isEqualTo(expectedStatus)
+    }
+
+    @Test
+    fun `given a live cell  with 1 live neighbor, should return as dead for next iteration`() {
+        val currentCellStatus = LIVE
+        val expectedStatus = DEAD
+        val neighbors = arrayOf(0,1,0,0,0,0,0,0)
+        val rules = GameOfLifeRule()
+
+        val actualStatus = rules.checkIsAlive(currentCellStatus,neighbors)
+
+        assertThat(actualStatus).isEqualTo(expectedStatus)
+    }
+
+    @Test
+    fun `given a live cell with 0 live neighbors, should return as dead for next iteration`() {
+        val currentCellStatus = LIVE
+        val expectedStatus = DEAD
+        val neighbors = arrayOf(0,0,0,0,0,0,0,0)
         val rules = GameOfLifeRule()
 
         val actualStatus = rules.checkIsAlive(currentCellStatus,neighbors)
